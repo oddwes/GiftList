1674d852c01162a1d3b2d6cf5f4a47cd61de432a
oddwes <oddwes777@gmail.com>
2024-04-28 19:17:15 -0700

    Implements name checking using MerkleTree

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const MerkleTree = require('../utils/MerkleTree');
+const niceList = require('../utils/niceList');
 const verifyProof = require('../utils/verifyProof');
 
 const port = 1225;
@@ -6,16 +8,16 @@ const port = 1225;
 const app = express();
 app.use(express.json());
 
-// TODO: hardcode a merkle root here representing the whole nice list
-// paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const merkleTree = new MerkleTree(niceList)
+const MERKLE_ROOT = merkleTree.getRoot();
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
-  const body = req.body;
+  const { name } = req.body;
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
   if(isInTheList) {
     res.send("You got a toy robot!");
   }
